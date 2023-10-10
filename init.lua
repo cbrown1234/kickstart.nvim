40fe39c41dc15980d6a34eaca87a1ea88d082de3
Chris Brown <cbrown1234@hotmail.co.uk>
2023-10-10 18:24:28 +0100

    Enable go

--- init.lua
+++ init.lua
@@ -448,7 +448,7 @@ end
 --  define the property 'filetypes' to the map in question.
 local servers = {
   -- clangd = {},
-  -- gopls = {},
+  gopls = {},
   -- pyright = {},
   rust_analyzer = {},
   -- tsserver = {},
